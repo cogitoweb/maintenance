3c12cb5d3cd62f6ab1978ceb0cd3d95d6638ce6d
Davide Viotto <davide.viotto@cogitoweb.it>
2025-07-07 10:45:06 +0200

    Filter non-string items from comparison

--- maintenance_plan/models/maintenance.py
+++ maintenance_plan/models/maintenance.py
@@ -209,11 +209,11 @@ class MaintenanceEquipment(models.Model):
                 ('stage_id.done', '!=', True),
                 ('close_date', '=', False)
             ]).mapped('request_date')
-            if len(next_plan_dates + next_unplanned_dates) <= 0:
+            all_dates = filter(lambda item: issubclass(item, str), next_plan_dates + next_unplanned_dates)
+            if len(all_dates) <= 0:
                 equipment.next_action_date = None
             else:
-                equipment.next_action_date = min(next_plan_dates +
-                                                 next_unplanned_dates)
+                equipment.next_action_date = min(all_dates)
 
 
 class MaintenanceRequest(models.Model):
